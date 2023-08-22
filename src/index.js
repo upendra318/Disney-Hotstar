511dc4b2a7846bb0a2d60c875a416564a08b0693
upendra318 <71498773+upendra318@users.noreply.github.com>
2023-08-23 00:41:16 +0530

    four Version

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './app/store';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render((
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
+  </React.StrictMode>),
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
